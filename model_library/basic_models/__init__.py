c4b77220917bd098396000e872cd17f8f5e60306
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2022-10-19 16:43:43 +0200

    removing chorin projection

--- model_library/basic_models/__init__.py
+++ model_library/basic_models/__init__.py
@@ -1,6 +1,4 @@
 from .basemodel import basemodel
 from .basemodel_linear_decoupled import basemodel_linear_decoupled
-from .basemodel_linear_decoupled_chorin import basemodel_linear_decoupled_chorin
 from .basemodel_linear_fp_decoupled import basemodel_linear_fp_decoupled
-from .basemodel_linear_fp_decoupled_chorin import basemodel_linear_fp_decoupled_chorin
 from .assist_funcs import *
