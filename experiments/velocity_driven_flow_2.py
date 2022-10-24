fc9e157990c65088a7f66aba8378ac288d30d115
Max Reiter <87012332+Max-Reiter-math@users.noreply.github.com>
2022-10-24 20:26:28 +0200

    Upscaled ics, changed bcs to model

--- experiments/velocity_driven_flow_2.py
+++ experiments/velocity_driven_flow_2.py
@@ -7,8 +7,7 @@ from fenics import *
 import numpy as np
 
 class velocity_driven_flow_2:
-    # as experiment one but in 2D
-    # as in Becker et. al
+
     def __init__(self, dim=3, n=2**4, dt=0.0005, T=0.5):
         self.name="velocity driven flow 2D"
         if dim in [2,3]: 
@@ -26,22 +25,22 @@ class velocity_driven_flow_2:
         if self.dim ==3:
             self.mesh = BoxMesh(Point(-0.5, -0.5,-0.5), Point(0.5, 0.5,0.5),n,n,n)
             self.boundary = boundary_3d
-            v_expr = Expression(("-10.0*x[1]","10.0*x[0]","0"), degree=2)
+            v_expr = Expression(("-20.0*x[1]","20.0*x[0]","0"), degree=2)
             zero_expr = Expression(("0.0","0.0","0"), degree=2)
             d0_expr = d0_expr_3d
         else:
             self.mesh = RectangleMesh(Point(-0.5,-0.5),Point(0.5,0.5),n,n)
             self.boundary = boundary_2d
-            v_expr = Expression(("-10.0*x[1]","10.0*x[0]"), degree=1)
+            v_expr = Expression(("-20.0*x[1]","20.0*x[0]"), degree=1)
             zero_expr = Expression(("0.0","0.0"), degree=2)
             d0_expr = d0_expr_2d
 
         # - initial conditions
         self.ics = [v_expr,Constant(0.0),d0_expr(),zero_expr]
         # boundary conditions
-        self.bcs = [v_expr, Constant(0.0), None, None]
+        self.bcs = [zero_expr, Constant(0.0), d0_expr(), zero_expr]
 
-        # - parameters namely, v_el, const_A
+        
 def boundary_2d(x):
             return x[0] < (DOLFIN_EPS -0.5) or x[0] > (0.5 - DOLFIN_EPS) or x[1] < (DOLFIN_EPS -0.5) or x[1] > (0.5 - DOLFIN_EPS)
         
@@ -49,7 +48,6 @@ def boundary_3d(x):
             return x[0] < (DOLFIN_EPS -0.5) or x[0] > (0.5 - DOLFIN_EPS) or x[1] < (DOLFIN_EPS -0.5) or x[1] > (0.5 - DOLFIN_EPS) or x[2] < (DOLFIN_EPS -0.5) or x[2] > (0.5 - DOLFIN_EPS)  
         
 # - define custom Expression
-# see https://fenicsproject.discourse.group/t/problem-defining-initial-conditions-expression-problem/626
 class d0_expr_2d(UserExpression):
     def eval(self,values,x):
         eta = 0.05
